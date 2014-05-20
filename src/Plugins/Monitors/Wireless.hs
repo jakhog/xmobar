df6775f860ab9e084ad0ec78d27d19175342cc0d
Jakob Høgenes <jakhoe@kompan.com>
2014-05-20 16:33:53 -0700

    Hello

--- src/Plugins/Monitors/Wireless.hs
+++ src/Plugins/Monitors/Wireless.hs
@@ -47,7 +47,7 @@ wirelessConfig =
 
 getIcon :: Maybe String -> Integer -> Float -> String
 getIcon Nothing _ _ = ""
-getIcon s _ _ = s
+getIcon s _ _ = Just s
 
 runWireless :: String -> [String] -> Monitor String
 runWireless iface args = do
